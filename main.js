d1838005c6e87a034dd1b884ec2de00f2d212db3
christiantondo <christiantondo1@gmail.com>
2025-10-30 09:03:33 +0100

    creazione prompt iniziali

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("JS Funzionante")
+
+// Input:
+// Recupero: numero di chilometri ed età del passeggero
+const age = parseInt(prompt("Inserisci l'età del viaggiatore"));
+const distance = parseInt(prompt("Inserisci la distanza da percorrere in chilometri"));
+
+// Elaborazione:
+// Calcolo tariffa di base (distanza * costo chilometrico)
+// Applico eventuali sconti
+// Verifico se l'utente è in una fascia d'età con scontistica
+// Eventualmente applico lo sconto (cioè ricalcolo il totale)
+console.log("Input age: ", age);
+console.log("Input distance: ", distance);
+//Output:
