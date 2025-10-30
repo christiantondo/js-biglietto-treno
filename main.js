9db7dbad7353664f0554ecb31702c7b98e785c08
christiantondo <christiantondo1@gmail.com>
2025-10-30 09:16:23 +0100

    logica di base per scontistica

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ console.log("JS Funzionante")
 // Recupero: numero di chilometri ed età del passeggero
 const age = parseInt(prompt("Inserisci l'età del viaggiatore"));
 const distance = parseInt(prompt("Inserisci la distanza da percorrere in chilometri"));
+const costoKM = 0.21;
 
 // Elaborazione:
 // Calcolo tariffa di base (distanza * costo chilometrico)
@@ -12,4 +13,17 @@ const distance = parseInt(prompt("Inserisci la distanza da percorrere in chilome
 // Eventualmente applico lo sconto (cioè ricalcolo il totale)
 console.log("Input age: ", age);
 console.log("Input distance: ", distance);
+
+const costoBiglietto = distance * costoKM;
+console.log("Costo del biglietto: ", costoBiglietto);
+
+if ( age < 18 ) {
+    console.log("Applicare scontistica Junior");
+} else if ( age > 65 ) {
+    console.log("Applicare scontistica Senior");
+} else {
+    console.log("Tariffa di base");
+}
+
 //Output:
+alert(`Il costo del biglietto è di ${costoBiglietto} €`);
