726f7da7584d55d81ef12b03468dbad2fbc1687e
christiantondo <christiantondo1@gmail.com>
2025-10-30 09:39:45 +0100

    aggiungo logica sconto

--- main.js
+++ main.js
@@ -5,25 +5,31 @@ console.log("JS Funzionante")
 const age = parseInt(prompt("Inserisci l'età del viaggiatore"));
 const distance = parseInt(prompt("Inserisci la distanza da percorrere in chilometri"));
 const costoKM = 0.21;
-
-// Elaborazione:
-// Calcolo tariffa di base (distanza * costo chilometrico)
-// Applico eventuali sconti
-// Verifico se l'utente è in una fascia d'età con scontistica
-// Eventualmente applico lo sconto (cioè ricalcolo il totale)
 console.log("Input age: ", age);
 console.log("Input distance: ", distance);
 
-const costoBiglietto = distance * costoKM;
-console.log("Costo del biglietto: ", costoBiglietto);
+// Elaborazione:
+// Calcolo tariffa di base (distanza * costo chilometrico)
+const costoBigliettoBase = distance * costoKM;
+console.log("Costo del biglietto: ", costoBigliettoBase);
 
+// Applico eventuali sconti
+let costoBigliettoFinale = null;
+
+// Verifico se l'utente è in una fascia d'età con scontistica
 if ( age < 18 ) {
+    costoBigliettoFinale = costoBigliettoBase * 0.80;
     console.log("Applicare scontistica Junior");
 } else if ( age > 65 ) {
+    costoBigliettoFinale = costoBigliettoBase * 0.60;
     console.log("Applicare scontistica Senior");
 } else {
+    costoBigliettoFinale = costoBigliettoBase;
     console.log("Tariffa di base");
 }
+// Eventualmente applico lo sconto (cioè ricalcolo il totale)
+
+
 
 //Output:
-alert(`Il costo del biglietto è di ${costoBiglietto} €`);
+alert(`Il costo del biglietto è di ${costoBigliettoFinale} €`);
